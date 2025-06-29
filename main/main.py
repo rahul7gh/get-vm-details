ab7ce74235efc97e55b63a085b6a52941428a7b4
rahul7gh <30512797+rahul7gh@users.noreply.github.com>
2025-06-29 12:21:33 +0530

    Update main.py

--- main/main.py
+++ main/main.py
@@ -7,7 +7,7 @@ parser.add_argument("--instance_type", type=str, help="instance_type")
 parser.add_argument("--env", type=str, help="env")
 
 args = parser.parse_args()
-output={"ipaddress":args.ipaddress,"machine_name":args.machine_name,"cpu":"2","memory":"16384MB","disk":"131072MB","instance_type":args.instance_type,"env":args.env}
+output={"ipaddress":args.ipaddress,"machine_name":args.machine_name,"cpu":"2","memory":"16384MB","disk":"131072MB","instance_type":args.instance_type,"env":args.env,"status":"ON"}
 print(json.dumps({"output":output}))
 
 
